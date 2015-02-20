16a7c021e509cd264d1ee596c892a333ad8e7e05
Edoardo Farnioli <edoardo.farnioli@iit.it>
2015-02-20 17:02:47 +0100

    Reference joint variables Computation

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 % How To Run the Module:
 % 
 % 
-% Open 4 terminal:
+% Open 5 terminal:
 % 
 % On the simulator:
 % Terminal 1. Run yarpserver
@@ -17,10 +17,14 @@
 % >> roscore
 % 
 % Terminal 3. Run Gazebo
-% >> gazebo
+% >> gazebo 
+% 
+% Insert a Coman in the world
 % 
 % Terminal 4. Run the Module!!
 % >> locomanipulation
+%
+% Terminal 5. Start (or quit) the Module!!
 % >> yarp write ... /generic_locoman/switch:i
 %    start
 %   quit
--- src/locoman_control_thread.cpp
+++ src/locoman_control_thread.cpp
@@ -1,9 +1,15 @@
 #include <yarp/os/all.h>
 #include <cstdlib>
-
+#include <yarp/math/Math.h>
+//#include <yarp/sig/all.h>
 #include "locoman_control_thread.h"
 #include "locoman_constants.h"
 #include <GYM/yarp_command_interface.hpp>
+
+
+using namespace yarp::math;
+
+
 
 locoman_control_thread::locoman_control_thread( std::string module_prefix, 
                              			yarp::os::ResourceFinder rf, 
@@ -31,16 +37,187 @@ void locoman_control_thread::link_locoman_params()
 
 bool locoman_control_thread::custom_init()
 {   
+    struct sched_param thread_param;
+    thread_param.sched_priority = 99;
+    pthread_setschedparam(pthread_self(), SCHED_FIFO, &thread_param);
+    
     link_locoman_params();
-    robot.setPositionMode();
-    robot.setReferenceSpeed(0.2);
+    
+    robot.idynutils.setFloatingBaseLink("base_link");
+    yarp::sig::Vector q_current = robot.sensePosition();
+    robot.idynutils.updateiDyn3Model(q_current, true);
+    
+    robot.setPositionDirectMode();
     return true;
 }
 
 void locoman_control_thread::run()
 {   
-    std::string cmd = command_interface.getCommand();
-   yarp::sig::Vector q_sensed = robot.left_arm.sensePosition();
+    
+    yarp::sig::Vector q_current = robot.sensePosition() ;
+    robot.idynutils.updateiDyn3Model( q_current, true ); //update model first
+    
+    yarp::sig::Vector tau_current = robot.senseTorque() ;
+
+    double a =1.0/1000.0 ; 
+
+    yarp::sig::Vector q_current_right_arm(robot.right_arm.getNumberOfJoints()) ; 
+    yarp::sig::Vector q_current_left_arm(robot.left_arm.getNumberOfJoints()) ;
+    yarp::sig::Vector q_current_torso(robot.torso.getNumberOfJoints()) ;
+    yarp::sig::Vector q_current_right_leg(robot.right_leg.getNumberOfJoints()) ;
+    yarp::sig::Vector q_current_left_leg(robot.left_leg.getNumberOfJoints()) ;
+    //
+    robot.fromIdynToRobot(  q_current,
+                            q_current_right_arm,
+                            q_current_left_arm,
+                            q_current_torso,
+                            q_current_right_leg,
+                            q_current_left_leg  ) ;
+			    
+    yarp::sig::Vector tau_current_right_arm(robot.right_arm.getNumberOfJoints()) ; 
+    yarp::sig::Vector tau_current_left_arm(robot.left_arm.getNumberOfJoints()) ;
+    yarp::sig::Vector tau_current_torso(robot.torso.getNumberOfJoints()) ;
+    yarp::sig::Vector tau_current_right_leg(robot.right_leg.getNumberOfJoints()) ;
+    yarp::sig::Vector tau_current_left_leg(robot.left_leg.getNumberOfJoints()) ;
+  
+    robot.fromIdynToRobot(  tau_current,
+                            tau_current_right_arm,
+                            tau_current_left_arm,
+                            tau_current_torso,
+                            tau_current_right_leg,
+                            tau_current_left_leg ) ;
+
+    yarp::sig::Vector q_ref_right_arm(robot.right_arm.getNumberOfJoints()) ; 
+    yarp::sig::Vector q_ref_left_arm(robot.left_arm.getNumberOfJoints()) ;
+    yarp::sig::Vector q_ref_torso(robot.torso.getNumberOfJoints()) ;
+    yarp::sig::Vector q_ref_right_leg(robot.right_leg.getNumberOfJoints()) ;
+    yarp::sig::Vector q_ref_left_leg(robot.left_leg.getNumberOfJoints()) ;
+    yarp::sig::Vector q_ref(robot.getNumberOfJoints()) ;
+  //  q_ref_left_arm[0] += .01;
+    
+    q_ref_torso = q_current_torso ;  //  (1/1000)*tau_current_torso + q_current_torso 
+    
+    q_ref_right_arm  =  q_current_right_arm	;
+    q_ref_left_arm   =  q_current_left_arm 	;
+    q_ref_right_leg  =  q_current_right_leg 	;
+    q_ref_left_leg   =  q_current_left_leg 	;
+    
+    
+    yarp::sig::Matrix C_right_arm( robot.right_arm.getNumberOfJoints() ,  robot.right_arm.getNumberOfJoints() ) ;
+    yarp::sig::Matrix C_left_arm(  robot.left_arm.getNumberOfJoints()  ,  robot.left_arm.getNumberOfJoints() ) ;
+    yarp::sig::Matrix C_torso(     robot.torso.getNumberOfJoints()     ,  robot.torso.getNumberOfJoints() ) ;
+    yarp::sig::Matrix C_right_leg( robot.right_leg.getNumberOfJoints() ,  robot.right_leg.getNumberOfJoints() ) ;
+    yarp::sig::Matrix C_left_leg(  robot.left_leg.getNumberOfJoints()  ,  robot.left_leg.getNumberOfJoints() ) ;
+
+    
+    //---------------------------------------------------------------------------//
+    // RIGHT ARM
+    
+    yarp::sig::Vector C_vec_right_arm(  robot.right_arm.getNumberOfJoints() ) ;
+   
+    C_vec_right_arm[0] = 1/1000 ;
+    C_vec_right_arm[1] = 1/1000 ;
+    C_vec_right_arm[2] = 1/600 ;
+    C_vec_right_arm[3] = 1/1000 ;
+    C_vec_right_arm[4] = 1/100 ;
+    C_vec_right_arm[5] = 1/100 ;
+    C_vec_right_arm[6] = 1/10 ;
+
+    C_right_arm.diagonal( C_vec_right_arm );
+    
+    //---------------------------------------------------------------------------//
+    // LEFT ARM
+    
+    yarp::sig::Vector C_vec_left_arm(  robot.left_arm.getNumberOfJoints() ) ;
+   
+    C_vec_left_arm[0] = 1/1000 ;
+    C_vec_left_arm[1] = 1/1000 ;
+    C_vec_left_arm[2] = 1/600 ;
+    C_vec_left_arm[3] = 1/1000 ;
+    C_vec_left_arm[4] = 1/100 ;
+    C_vec_left_arm[5] = 1/100 ;
+    C_vec_left_arm[6] = 1/10 ;
+
+    C_left_arm.diagonal( C_vec_left_arm );
+     
+    //--------------------------------------------------------------------------//
+    //TORSO
+    
+    yarp::sig::Vector C_vec_torso(  robot.torso.getNumberOfJoints() ) ;
+   
+    C_vec_torso[0] = 1/1000 ;
+    C_vec_torso[1] = 1/1000 ;
+    C_vec_torso[2] = 1/1000 ;
+
+
+    C_torso.diagonal( C_vec_torso );
+
+
+    //---------------------------------------------------------------------------//
+    // RIGHT LEG
+    
+    yarp::sig::Vector C_vec_right_leg(  robot.right_leg.getNumberOfJoints() ) ;
+   
+    C_vec_right_leg[0] = 1/3000 ;
+    C_vec_right_leg[1] = 1/5000 ;
+    C_vec_right_leg[2] = 1/3000 ;
+    C_vec_right_leg[3] = 1/3000 ;
+    C_vec_right_leg[4] = 1/4000 ;
+    C_vec_right_leg[5] = 1/3000 ;
+
+    C_right_leg.diagonal( C_vec_right_leg );
+    
+    //---------------------------------------------------------------------------//
+    // LEFT LEG
+    
+    yarp::sig::Vector C_vec_left_leg(  robot.left_leg.getNumberOfJoints() ) ;
+   
+    C_vec_left_leg[0] = 1/3000 ;
+    C_vec_left_leg[1] = 1/5000 ;
+    C_vec_left_leg[2] = 1/3000 ;
+    C_vec_left_leg[3] = 1/3000 ;
+    C_vec_left_leg[4] = 1/4000 ;
+    C_vec_left_leg[5] = 1/3000 ;
+
+    C_left_arm.diagonal( C_vec_left_arm );    
+    
+    //---------------------------------------------------------------------------//
+
+    q_ref_right_arm =  C_right_arm*tau_current_right_arm +  q_current_right_arm ;
+    q_ref_left_arm  =  C_left_arm*tau_current_left_arm   +  q_current_left_arm ;
+    q_ref_torso     =  C_torso*tau_current_torso         +  q_current_torso ; //   q_current_torso ; 
+    q_ref_right_leg =  C_right_leg*tau_current_right_leg +  q_current_right_leg ;
+    q_ref_left_leg  =  C_left_leg*tau_current_left_leg   +  q_current_left_leg ;   
+    
+    
+    
+    robot.fromRobotToIdyn( q_ref_right_arm ,
+                           q_ref_left_arm  ,
+                           q_ref_torso  ,
+                           q_ref_right_leg ,
+                           q_ref_left_leg  ,
+                           q_ref ); 
+    //---------------------------------------------------------------------------//
+    // Test Printing
+    
+    std::cout << "q_ref_torso[0]" << q_ref_torso[0] << endl ;
+                           
+                           
+                           
+  //Try to stay in the current configuration
+  
+     //  yarp::sig::Vector q_ref =  a*tau_current + q_current ;
+  
+        // Move something
+//     yarp::sig::Vector q_ref = q_current ;
+//     q_ref[0] += .1 ;
+
+    // robot.move(q_ref);
+    
+    //     std::string cmd = command_interface.getCommand() ;  // USEFUL TO SEND INPUT TO THE MOUDLE
+
+    /*  Code from the generic module
+    yarp::sig::Vector q_sensed = robot.left_arm.sensePosition();
     std::cout << "Sense : " << q_sensed.toString() << std::endl;
     
     yarp::sig::Vector q_ref(left_arm_joints, 0.0);
@@ -48,11 +225,11 @@ void locoman_control_thread::run()
 	q_ref[i] = q_sensed[i] + sin( (omega * tick / 5) + phi );
     }
     
-    std::cout << "Rererence : " << q_ref.toString() << std::endl;
+    std::cout << "Reference : " << q_ref.toString() << std::endl;
     
     robot.left_arm.move(q_ref);
-    
     tick++;
+    */
 }
 
 bool locoman_control_thread::custom_pause()
