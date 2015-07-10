d4dbdcfc9943bb3731506e8306850961dc54f0b1
Luca Muratore <luca.muratore@iit.it>
2015-07-10 11:31:28 +0200

    removed build and kdev4 files

--- build/app/CMakeFiles/copy_conf_files_to_build.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/copy_conf_files_to_build"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/copy_conf_files_to_build.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
