d37a86153ef3d7f59760a392125762b6657e970e
Edoardo Farnioli <edoardo.farnioli@iit.it>
2016-01-14 12:40:27 +0100

    wrong version of the build folder removed

--- build/only_feet/app/CMakeFiles/only_feet_copy_conf_files_to_build.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/only_feet_copy_conf_files_to_build"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/only_feet_copy_conf_files_to_build.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
